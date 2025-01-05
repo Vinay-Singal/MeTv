4c011037e619458ef461166c2920bc4b74bd5967
VINAY SINGAL <vinaysingal123ghn@gmail.com>
2025-01-05 12:09:31 +0530

    Update 1+.php

--- 1+.php
+++ 1+.php
@@ -10,7 +10,7 @@ require("connection.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="homepagecss.css">
-    <title>OnePlus</title>
+    <title>OnePlus tv</title>
 
     <style>
          header {
