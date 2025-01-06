744d86535d206faebd06b7edc07d7b4f65e46ed2
VINAY SINGAL <vinaysingal123ghn@gmail.com>
2025-01-06 18:32:23 +0530

    first commit

--- 1+.php
+++ 1+.php
@@ -10,7 +10,7 @@ require("connection.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="homepagecss.css">
-    <title>OnePlus tv</title>
+    <title>OnePlus</title>
 
     <style>
          header {
